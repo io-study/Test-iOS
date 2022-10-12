fdecb16dbfe4194bf4f0a36782121269cacb890a
junseopark0331 <junseopark0331@icloud.com>
2022-10-12 23:39:18 +0900

    원래는 회원가입 화면을 만들어보려고 했는데, 약간 흐름이 로그인 화면으로 가서 결국 최종적으로는 로그인 화면이 만들어졌다. 하나하나 구글링과 형우형의 도움으로 만든거라 형우형에게 감사하고 또 한편으로는 뿌듯하다! 미소 1팀 기대해라.

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -6,12 +6,55 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    let idTextField = UITextField()
+    let psTextField = UITextField()
+    
+    let loginImageView = UIImageView()
 
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        view.addSubview(idTextField)
+        view.addSubview(psTextField)
+        view.addSubview(loginImageView)
+        
+        idTextField.borderStyle = .line //모서리 둥글게
+        idTextField.borderStyle = .roundedRect
+        idTextField.placeholder = "아이디를 입력해주세요!"
+        
+        psTextField.borderStyle = .line
+        psTextField.borderStyle = .roundedRect
+        psTextField.placeholder = "비밀번호를 입력해주세요!"
+        
+        loginImageView.image = UIImage(named: "HI_Logo")
+        loginImageView.layer.cornerRadius = 50 //사진 겉에 둥글게
+        loginImageView.clipsToBounds = true // 잘리는거
+        
+        idTextField.snp.makeConstraints {
+            $0.centerX.equalTo(view) //뷰에서의 X좌표의 가운데
+            $0.centerY.equalTo(view).offset(80)
+            $0.width.equalTo(300) //너비 = 60
+            $0.height.equalTo(50) // 높이 = 10
+            
+        }
+        psTextField.snp.makeConstraints {
+            $0.centerX.equalTo(view)
+            $0.centerY.equalTo(view).offset(150)
+            $0.width.equalTo(300)
+            $0.height.equalTo(50)
+        }
+        loginImageView.snp.makeConstraints {
+            $0.centerX.equalTo(view)
+            $0.centerY.equalTo(view).offset(-150)
+            $0.width.equalTo(300)
+            $0.height.equalTo(300)
+        }
+        
     }
 
 
